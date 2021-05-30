906489d08f99aa17060b447e33a020895d0f1fbf
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-05-30 21:59:28 +0200

    Update config.cs

--- config.cs
+++ config.cs
@@ -1,4 +1,4 @@
-﻿using Exiled.API.Interfaces;
+using Exiled.API.Interfaces;
 using System.ComponentModel;
 
 namespace BetterConsoleLogs
@@ -7,22 +7,7 @@ namespace BetterConsoleLogs
     {
         public bool IsEnabled { get; set; } = true;
 
-        [Description("Sets the log message when someone join the server. ({player} will be replaced with the players name.)")]
-        public string JoinedMessage { get; set; } = "{player} has joined the server!";
-
-        [Description("Sets the log message when someone leaves the server. ({player} will be replaced with the players name.)")]
-        public string LeftMessage { get; set; } = "{player} has left the server.";
-
-        [Description("Sets the log message when someone Interact with a door. ({player} will be replaced with the players name.)")]
-        public string OnInteractingDoor { get; set; } = "{player} has left the server.";
-
-        [Description("Sets the log message when someone activate a generator. ({player} will be replaced with the players name.)")]
-        public string OnActivatingGenerator { get; set; } = "{player} has left the server.";
-
-        [Description("Sets the log message when someone call an elevator. ({player} will be replaced with the players name.)")]
-        public string OnCallingElevator { get; set; } = "{player} has called an elevator.";
-
-        [Description("Sets the log message when someone speak at the intercom. ({player} will be replaced with the players name.)")]
-        public string OnSpeakingIntercom { get; set; } = "{player} has activate the intercom.";
+        [Description("are RA command logs enabled?")]
+        public bool CommandLogs { get; set; } = false;
     }
 }
