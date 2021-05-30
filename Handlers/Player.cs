0397366c15313025aeebaf6d51fde35f4abeeac8
AleRabo <83182793+AleRabo@users.noreply.github.com>
2021-05-30 21:55:34 +0200

    Update Player.cs

--- Handlers/Player.cs
+++ Handlers/Player.cs
@@ -8,36 +8,30 @@ namespace BetterConsoleLogs.Handlers
     {
         public void OnLeft(LeftEventArgs ev)
         {
-            string message = BetterConsoleLogs.Singleton.Config.LeftMessage.Replace("{player}", ev.Player.Nickname);
             Log.Info($"{ev.Player} has left the server");
         }
 
         public void OnVerified(VerifiedEventArgs ev)
         {
-            string message = BetterConsoleLogs.Singleton.Config.JoinedMessage.Replace("{player}", ev.Player.Nickname);
             Log.Info($"{ev.Player} has joined the server");
         }
 
         public void OnInteractingDoor(InteractingDoorEventArgs ev)
         {
-            string message = BetterConsoleLogs.Singleton.Config.OnInteractingDoor.Replace("{player}", ev.Player.Nickname);
             Log.Info($"{ev.Player} has interacted with door");
         }
 
         public void OnActivatingGenerator(InsertingGeneratorTabletEventArgs ev)
         {
-            string message = BetterConsoleLogs.Singleton.Config.OnActivatingGenerator.Replace("{player}", ev.Player.Nickname);
             Log.Info($"{ev.Player} has activated a generator");
         }
 
         public void OnCallingElevator(InteractingElevatorEventArgs ev)
         {
-            string message = BetterConsoleLogs.Singleton.Config.OnCallingElevator.Replace("{player}", ev.Player.Nickname);
             Log.Info($"{ev.Player} has called an elevator");
         }
         public void OnSpeakingIntercom(IntercomSpeakingEventArgs ev)
         {
-            string message = BetterConsoleLogs.Singleton.Config.OnSpeakingIntercom.Replace("{player}", ev.Player.Nickname);
             Log.Info($"{ev.Player} has start the intercom");
         }
     }
